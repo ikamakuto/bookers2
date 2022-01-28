9caa6e283dd79a09a8596f12b943f8871d9b2370
EC2 Default User <ec2-user@ip-172-31-89-204.ec2.internal>
2022-01-28 17:03:07 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
   resources :profile_images, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :books
-resources :users, only: [:show]
+resources :users, only: [:show, :edit]
 end
